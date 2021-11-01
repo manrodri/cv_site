f95615a63377f73764779fe424294d83d4ba40bf
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2021-11-01 09:07:38 +0000

    adding webhook to cb project

--- ci-cd/main.tf
+++ ci-cd/main.tf
@@ -115,3 +115,19 @@ resource "aws_codebuild_project" "example" {
 
   tags = local.common_tags
 }
+
+resource "aws_codebuild_webhook" "example" {
+  project_name = aws_codebuild_project.example.name
+  build_type   = "BUILD"
+  filter_group {
+    filter {
+      type    = "EVENT"
+      pattern = "PUSH"
+    }
+
+    filter {
+      type    = "HEAD_REF"
+      pattern = "master"
+    }
+  }
+}
